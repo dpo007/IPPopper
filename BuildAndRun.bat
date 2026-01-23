4b06a8546786ae5b43a2c68b47b3f00a599db4ee
techie007 <11600782+dpo007@users.noreply.github.com>
2026-01-23 01:16:14 -0500

    Remove BuildAndRun.bat and RunIPPopper.bat scripts
    Both batch files for building/running IPPopper in Release and Debug modes have been deleted, leaving them empty. These scripts are no longer needed.

--- BuildAndRun.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Building IPPopper...
-dotnet build -c Release
-
-echo.
-echo Running IPPopper...
-cd /d "%~dp0"
-start "" "bin\Release\net9.0-windows\IPPopper.exe"
--- RunIPPopper.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "%~dp0"
-start "" "bin\Debug\net9.0-windows\IPPopper.exe"
